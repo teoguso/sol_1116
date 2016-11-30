64d0ae0271893ed7e8387500b90497914543aa54
Matteo Guzzo <guzzo.matteo@gmail.com>
2016-11-30 16:15:34 +0100

    Added html_embed for the slides

--- convert-slides.sh
+++ convert-slides.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 jupyter-nbconvert --to slides slides.ipynb --reveal-prefix=reveal.js
-cp slides.slides.html index.html
+# Uncomment and use to produce slides.html,
+# a version of the slides with embedded media.
+# Requires nbconvert_nbextensions
+# jupyter-nbconvert --to html_embed slides.ipynb
+
+# Not useful anymore
+# cp slides.slides.html index.html
